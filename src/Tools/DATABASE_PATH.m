4363982c8300b853126c3352ba49e8089567d6d7
Ali A Eftekhari <e.eftekhari@gmail.com>
2019-10-10 15:35:32 +0200

    fixed a bug in database path

--- src/Tools/DATABASE_PATH.m
+++ src/Tools/DATABASE_PATH.m
@@ -3,6 +3,6 @@ function db_path = DATABASE_PATH()
 p = mfilename('fullpath');
 file_name = mfilename;
 current_path = p(1:end-1-length(file_name));
-db_path = fullfile(current_path, '..', '/database');
+db_path = fullfile(current_path, '..', '..', '/database');
 end
 
