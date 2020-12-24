dfb36cc76d8c7823f33a823be7746ea2e671c7bf
Ali A Eftekhari <e.eftekhari@gmail.com>
2020-12-24 01:06:20 +0100

    slightly better tool to read input files

--- src/Tools/ReadPhreeqcFile.m
+++ src/Tools/ReadPhreeqcFile.m
@@ -1,15 +1,21 @@
 function C = ReadPhreeqcFile(input_file)
-%READPHREEQCFILE Reads a phreeqc input file, converts it to a matlab
-%string, and clean up the comments
-%returns a cell array of strings
-fid = fopen(input_file, 'r');
-C = textscan(fid, '%s','Delimiter',''); % read the input file into a cell array
-fclose(fid);
-C = C{:}; % get rid of cells in cell C
+    %READPHREEQCFILE Reads a phreeqc input file, converts it to a matlab
+    %string, and clean up the comments
+    %returns a cell array of strings
+    fid = fopen(input_file, 'r');
+    C = textscan(fid, '%s','Delimiter',''); % read the input file into a cell array
+    fclose(fid);
+    C = C{:}; % get rid of cells in cell C
 
-% clean the comments:
-ind_comment = cellfun(@(x)(x(1)=='#'), C);
-C(ind_comment) = {' '};
+    % clean the comments:
+    ind_comment = cellfun(@(x)(x(1)=='#'), C);
+    C(ind_comment) = []; % simply remove the comment lines
+
+    % remove extra spaces
+    for i=1:length(C)
+        C{i}(strfind(C{i}, '#'):end) = '';
+        C{i} = strtrim(C{i});
+    end
 
 end
 
