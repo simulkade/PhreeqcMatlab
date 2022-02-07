bbb0701b83e19dbd2c63dbd125f738eea8f99433
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-02-07 11:24:09 +0100

    Update SingleCell.m

--- src/@SingleCell/SingleCell.m
+++ src/@SingleCell/SingleCell.m
@@ -1,9 +1,17 @@
 classdef SingleCell
     %SINGLECELL is a class that contains a mixture of different components.
     % it contains at least an aqueous solution. It can contain solid and
-    % gas phases too, which are defined as PhreeqcMatlab objects.
+    % gas phases too, which are defined as PhreeqcMatlab objects. It also
+    % has fields for temperature and pressure.
+    % The reaction rate field can also be specified to run the cell for a
+    % certain specified time. Running a cell does not update its fields
+    % (i.e. equilibrate aqueous solutions with other specified phases, etc)
+    % but one can get a copy of an aqueous cell after running it with
+    % PhreeqcRM. The copy is saved as a PhreeqcCell class.
     
     properties
+        temperature
+        pressure
         aqueous_solution
         equilibrium_phase
         surface_specie
