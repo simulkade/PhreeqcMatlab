a54f1d2e67d118eddbf08d9b4910e7298c1a7174
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-03-25 14:31:36 +0100

    fixed a bug in read advection function

--- src/Advection1D/ReadAdvectionFile.m
+++ src/Advection1D/ReadAdvectionFile.m
@@ -38,6 +38,7 @@ end
 use_solution_density_volume = sscanf(C{contains(C, 'use_solution_density_volume')}, 'use_solution_density_volume %d');
 if isempty(use_solution_density_volume)
     use_solution_density_volume = 0;
+end
 % endprimary_species = 
 units_solution = sscanf(C{contains(C, 'units_solution')}, 'units_solution %d');
 if isempty(units_solution)
