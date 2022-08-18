748a19faea4c26ce6a670f3f2dc2e03cc525dc30
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-08-18 15:37:30 +0200

    failed attempt for iphreeqc selected output

--- examples/IPhreeqc/IPhreeqc_test.m
+++ examples/IPhreeqc/IPhreeqc_test.m
@@ -2,9 +2,30 @@
 % Note that IPhree
 iph = IPhreeqc(); % load the library
 iph = iph.CreateIPhreeqc(); % create an IPhreeqc instance
+iph.SetSelectedOutputStringOn(1);
 iph.SetOutputStringOn(true);
 iph.LoadDatabase(database_file('phreeqc.dat'));
 iph.RunFile('ex2_input.pqc');
 out_string = iph.GetOutputString();
 disp(out_string); % display the phreeqc output string
-iph.DestroyIPhreeqc();
+% for i=1:iph.GetSelectedOutputCount
+n_so =1;
+iph.SetCurrentSelectedOutputUserNumber(n_so);
+n_row = iph.GetSelectedOutputRowCount();
+n_col = iph.GetSelectedOutputColumnCount();
+val = zeros(n_row-1, n_col);
+
+iph.GetSelectedOutputString()
+n=2;
+nPtr=libpointer('int64Ptr', n);
+c = 0;
+cPtr = libpointer('doublePtr', c);
+h = "aaaaaaaaaa";
+hPtr = libpointer('stringPtr', h);
+iph.GetSelectedOutputValue2(1, 1, nPtr, cPtr, hPtr, uint32(length(h)));
+% for 
+% for i=1:n_col
+%     iph.GetSelectedOutputValue2()
+% end
+
+% iph.DestroyIPhreeqc();
