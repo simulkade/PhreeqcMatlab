cbb6a4339a9e77a7609486a1dd23a4c4ec79db7f
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-11-18 16:08:05 +0100

    CCM not charge balanced

---
+++ examples/IPhreeqc/IPhreeqc_test3_SCM.m
@@ -0,0 +1,13 @@
+% A script that tests the functionality of the IPhreeqc wrapper
+% Note: IPhreeqc does not undeerstand salts as input to solutions
+iph = IPhreeqc(); % load the library
+iph = iph.CreateIPhreeqc(); % create an IPhreeqc instance
+out_string = iph.RunPhreeqcFile('CCM.pqi', database_file('phreeqc.dat'));
+disp(out_string)
+% the above function does the following:
+% iph.SetOutputStringOn(true);
+% iph.LoadDatabase(database_file('phreeqc.dat'));
+% iph.RunFile('IPhreeqc_test2.pqc');
+% out_string = iph.GetOutputString();
+% disp(out_string); % display the phreeqc output string
+iph.DestroyIPhreeqc();
